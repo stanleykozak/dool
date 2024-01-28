abee6ef01dd3c6fd2ee109210ec517b0a19e00c5
stanleykozak <92325523+stanleykozak@users.noreply.github.com>
2024-01-28 22:43:45 +1100

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -19,3 +19,12 @@ systemctl show --property SystemState --property NFailedUnits --property NNames 
 sort -h | \
 cut -d'=' -f2 | \
 tr '\n' ' ' > /tmp/dool_systemd
+
+if [[ ! -f /tmp/dool_checkupdates ]]; then 
+    touch /tmp/dool_checkupdates
+fi
+
+#CHECK UPDATES
+if [[ "$(date +%R)" == "01:00" ]] || [[ "$(date +%R)" == "01:01" ]] || [[ "$(date +%R)" == "0:59" ]]; then
+   apt list --upgradable 2>/dev/null | wc -l >/tmp/dool_checkupdates
+fi
